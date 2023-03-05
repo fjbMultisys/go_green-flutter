26e097173f11254ffbe376e334065f2438c2c9a9
fjbMultisys <fabergonia@multisyscorp.com>
2023-03-05 21:50:40 +0800

    Designed the To-Do List App

---
+++ android/app/src/main/kotlin/com/xiapps/todoey_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xiapps.todoey_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lime,
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+          },
+          backgroundColor: Colors.lime[500],
+          child: Icon(Icons.add_task)
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 10.0),
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0,
+                left: 30.0,
+                right: 30.0,
+                bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(child: Icon(Icons.energy_savings_leaf,
+                  size: 50.0,
+                  color: Colors.green,),
+                    backgroundColor: Colors.white,
+                    radius: 30.0),
+                SizedBox(height: 10.0),
+                Text('GoGreen',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text('12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Expanded(child:
+          Container(
+            height: 300.0,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                )
+            ),
+          ),
+          ),
+        ],
+      )
+      );
+  }
+}
